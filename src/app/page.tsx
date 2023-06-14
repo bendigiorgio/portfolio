738dbaa98b61bbba025e9c7857fe43a10769aedf
bendigiorgio <bendigiorgio@gmail.com>
2023-06-14 09:27:44 +0900

    var

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Artwork from "@/components/Artwork";
-
 import DesignNew from "@/components/DesignNew";
 import Expertise from "@/components/Expertise";
 import Landing from "@/components/Landing";
-import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
@@ -11,6 +8,7 @@ export default function Home() {
       <Landing />
       <DesignNew />
       <Expertise />
+
       {/* <Projects /> */}
     </main>
   );
